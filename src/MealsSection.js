365e5970b8ff2b9b5603ee6ec3786144eeca19ac
arpit2707 <arpitgaurav.goldi723@gmail.com>
2024-05-02 21:53:16 +0530

    Quantity Reaady

--- src/MealsSection.js
+++ src/MealsSection.js
@@ -14,12 +14,30 @@ const MealsSection = () => {
       <div className="p-4">
         {meals.map((items) => (
           <div>
-            <h5 className=" font-bold  ">{items.name}</h5>
-            <p className=" font-serif   ">
-              <i>{items.desc}</i>
-            </p>
-            <p className=" text-orange-950 font-bold ">{`Rs.` + items.price}</p>
-            <div className="border-b border-gray-300"></div>
+            <div className="flex justify-between">
+              <div>
+                <h5 className=" font-bold  ">{items.name}</h5>
+                <p className=" font-serif   ">
+                  <i>{items.desc}</i>
+                </p>
+                <p className=" text-orange-950 font-bold ">
+                  {`Rs.` + items.price}
+                </p>
+              </div>
+              <div className="items-end justify-center">
+                <div className="flex ">
+                  <p className="text-black m-1">Quantity</p>
+                  <input
+                    className="rounded-md w-10 h-6 m-1 bg-white border-black border-solid border-2     "
+                    type="number"
+                  />
+                </div>
+                <button className="rounded-md w-20 items-center font-bold m-2 h-6 text-white bg-orange-900 border-b border-gray-500 flex justify-center">
+                  + Add
+                </button>
+              </div>
+            </div>
+            <div className="border-b  border-gray-500"></div>
           </div>
         ))}
       </div>
